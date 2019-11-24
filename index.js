03a224e0951fce3ef38e0ea7fe895b162f9f3e14
LaWinTun <57011173+LaWinTun101@users.noreply.github.com>
2019-11-24 21:37:14 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 window.onload = function() {
+var center = document.createElement("center");
 var p = document.createElement("p");
 var node = document.createTextNode("Hello World");
 p.appendChild(node);
+center.appendChild(p);
 
 var body = document.getElementByTagName("body");
-body.appendChild(p);
+body.appendChild(center);
+
 };
