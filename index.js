d1e741e7a773d1e17a5d2f5940bcaaf41c25e845
LaWinTun <57011173+LaWinTun101@users.noreply.github.com>
2019-11-25 03:16:58 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-﻿document.write(" Comparing Two number ");
-
-function compare(){
-var vone = parseInt(document.getElementById("fn").value);
-var vtwo = parseInt(document.getElementById("sn").value);
-if (vone > vtwo) {
-  alert("First number is greater than second number");
- }
-else {
-  alert("Second number is greater than first number");
+function update(){
+var idoc = document.getElementById("iframe").contentWindow.document;
+idoc.open();
+idoc.write(editor.getValue());
+idoc.close();
 }
+
+function setupEditor(){
+window.editor = ace.edit("editor");
+editor.setTheme = ("ace/theme/monokai);
+editor.getSession().setMode("ace/mode/html");
+editor.setValue(`
+<`DOCTYPE  html>
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+</body>
+</html> ` , 1);
+
+editor.getSession().on('change',function()
+{
+update();
+});
+
+editor.focus();
+editor.setOptions({
+  fontfamily: "Monaco",
+  fontSize: "16pt",
+  showLineNumbers: false,
+  showGutter: false,
+  vScrollBarAlwaysVisible: true,
+  enableBasicAutocompletion: false,
+  enableLiveAutocompletion: false,
+});
+editor.setShowPrintMargin(false);
+editor.setBehaviorsEnabled(false);
+}
+
+function ready(){
+
+          setupEditor();
+          update();
+
 }
