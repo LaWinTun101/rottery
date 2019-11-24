51a57764ae57aa2a6e7371ba71ae2ce4cdcc0623
LaWinTun <57011173+LaWinTun101@users.noreply.github.com>
2019-11-24 16:41:15 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var ob = document.getElementByTagName("p");
-ob.innerHTML = "yun lay";
+var result = confirm("Do you really want to leave this page?");
+if (result == true) {
+    alert("Thanks for visiting");
+}
+else {
+    alert("Thanks for staying with us");
+}
