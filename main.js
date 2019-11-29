32c71724ee2a4755184f909cb58cd1d3d8c8445e
LaWinTun <57011173+LaWinTun101@users.noreply.github.com>
2019-11-30 00:57:35 +0630

    Delete main.js

--- main.js
+++
@@ -1,21 +0,0 @@
-//say hi
-function ready(){
-  alert("  Hello Mingalar Par \n\n  @825B8: 4@02AB2C9B5");
-}
-
-//font change
-function userFont(selectTag) {
-var listValue= selectTag.options[selectTag.selectedIndex].text;
-
-document.getElementById("bodyFont").style.fontFamily = listValue;
-}
-
-function more(){
-alert(" $/9  1,9 :9 -/  18,8,8 18+");
-}
-
-function next(){
-alert("$/9  1,9 :9 -/  18,8,8 18+");
-}
-
-
