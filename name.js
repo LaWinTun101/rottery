a41a52b7ae795bfb0da5b518aca6485defe28ad6
LaWinTun <57011173+LaWinTun101@users.noreply.github.com>
2019-11-30 15:07:04 +0630

    Update name.js

--- name.js
+++ name.js
@@ -1,5 +1,16 @@
 function init(){
+    alert ("Mingalar Par");
 
-alert("MIngalar Par");
+var x = document.getElementById("btnZawgyi");
+x.addEventListener('click',zgyFun);
+function zgyFun(){
+document.getElementById("zp").style.fontFamily="Zawgyi-One"
+;}
+window.onload =init;
+
+document.getElementById("btnUni").addEventListener ('click',uniFun);
+function uniFun (){
+    document.getElementById("zp").style.fontFamily ="Myanmar3,Yunghkio";
+}
 
 }
